1ed915605af7638a8cb6e26eb5ff61d27c2736c8
Kathia Mgwaba <mgwaba68@gmail.com>
2019-11-10 18:07:27 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -7,3 +7,12 @@ def reverse_each_word(string)
     new_array.join(" ")
 
 end
+
+def reverse_each_word(string)
+  split_string = string.split(" ")
+  new_array = []
+  split_string.each do |string|
+    new_array << string.reverse
+  end
+    new_array.join(" ")
+end
