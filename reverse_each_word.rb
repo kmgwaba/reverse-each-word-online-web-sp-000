ff22769256540e86ff93db9839c9c69856261bb6
Kathia Mgwaba <mgwaba68@gmail.com>
2019-11-10 18:08:10 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -10,9 +10,9 @@ end
 
 def reverse_each_word(string)
   split_string = string.split(" ")
-  new_array = []
+
   split_string.each do |string|
-    new_array << string.reverse
+    string.reverse
   end
-    new_array.join(" ")
+    
 end
